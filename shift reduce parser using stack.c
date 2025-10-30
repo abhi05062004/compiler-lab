219ab844f05cd984ee3dcc81b2f22e6c1c95b802
Abhijith V A <117969751+abhi05062004@users.noreply.github.com>
2025-10-30 23:17:05 +0530

    second commit

--- shift reduce parser using stack.c
+++ shift reduce parser using stack.c
@@ -1,66 +1,70 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
 #define MAX 50
 
 char str[MAX], stack[MAX];
-int top=-1;
+int top = -1;
 
-void push(char c){
-    stack[++top]=c;
-    stack[top+1]='\0';
+void push(char c) {
+    stack[++top] = c;
+    stack[top + 1] = '\0';
 }
 
-void pop(int n){
-    top-=n;
-    stack[top+1]='\0';
+void pop(int n) {
+    top -= n;
+    stack[top + 1] = '\0';
 }
 
-void check(){
-    char reduce[20];
+void check() {
     int reduced;
-    do{
-        reduced=0;
-        if(top>=0 && stack[top]=='4'){
-            strcpy(reduce,"REDUCED TO E->4");
-            printf("\n$%s\t%s$\t%s",stack,str,reduce);
-            stack[top]='E';
-            reduced=1;
-        } else if(top>=2 && stack[top]=='3' && stack[top-1]=='E' && stack[top-2]=='3'){
-            strcpy(reduce,"REDUCED TO E->3E3");
-            printf("\n$%s\t%s$\t%s",stack,str,reduce);
+    do {
+        reduced = 0;
+
+        if (top >= 0 && stack[top] == '4') {
+            pop(1);
+            push('E');
+            printf("$%s\t%s$\tReduce by E-->4\n", stack, str);
+            reduced = 1;
+        } 
+        else if (top >= 2 && stack[top] == '2' && stack[top - 1] == 'E' && stack[top - 2] == '2') {
             pop(3);
             push('E');
-            reduced=1;
-        } else if(top>=2 && stack[top]=='2' && stack[top-1]=='E' && stack[top-2]=='2'){
-            strcpy(reduce,"REDUCED TO E->2E2");
-            printf("\n$%s\t%s$\t%s",stack,str,reduce);
+            printf("$%s\t%s$\tReduce by E-->2E2\n", stack, str);
+            reduced = 1;
+        } 
+        else if (top >= 2 && stack[top] == '3' && stack[top - 1] == 'E' && stack[top - 2] == '3') {
             pop(3);
             push('E');
-            reduced=1;
+            printf("$%s\t%s$\tReduce by E-->3E3\n", stack, str);
+            reduced = 1;
         }
-    } while(reduced);
+    } while (reduced);
 }
 
-int main(){
-    int len;
-    printf("GRAMMAR IS \nE->2E2\nE->3E3\nE->4\n");
+int main() {
+    printf("GRAMMAR IS\nE->2E2\nE->3E3\nE->4\n");
     printf("Enter the input string:\t");
-    scanf("%s",str);
-    len=strlen(str);
-    printf("\nstack\tinput\tAction\n");
-    printf("\n$\t%s$\t",str);
-    for(int j=0;j<len;j++){
-        printf("SHIFT");
+    scanf("%s", str);
+
+    printf("\nStack\tInput Buffer\tParsing Action\n");
+    printf("$\t%s$\tShift\n", str);
+
+    int len = strlen(str);
+    for (int j = 0; j < len; j++) {
         push(str[j]);
-        str[j]=' ';
-        printf("\n$%s\t%s$\t",stack,str);
+        str[j] = ' ';
+        printf("$%s\t%s$\tShift\n", stack, str);
         check();
     }
+
     check();
-    printf("\n$%s\t%s$\t",stack,str);
-    if(top==0 && stack[top]=='E') printf("Accepted\n");
-    else printf("Rejected\n");
+
+    if (top == 0 && stack[top] == 'E')
+        printf("$%s\t%s$\tAccept\n", stack, str);
+    else
+        printf("$%s\t%s$\tReject\n", stack, str);
+
     return 0;
 }
-
